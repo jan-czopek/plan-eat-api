d37ea459ec407b756eb9b5e65d4fb676e681f07d
Jan Czopek <janczopek@hotmail.com>
2023-09-30 00:00:42 +0200

    add production client to whitelist

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,9 @@ const whiteList = [
   'http://127.0.0.1:3000',
   'http://localhost:3000',
   'http://127.0.0.1:3500',
-  'http://localhost:3500'
+  'http://localhost:3500',
+  'https://plan-eat-client.onrender.com/'
+
 ];
 const corsOptions = {
   origin: (origin, callback) => {
