9afacf3084156f36ce50d69a5dc68438fc16a2a6
Jan Czopek <janczopek@hotmail.com>
2023-09-30 01:09:08 +0200

    whitelist edit

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,7 @@ const whiteList = [
   'http://localhost:3000',
   'http://127.0.0.1:3500',
   'http://localhost:3500',
-  'https://plan-eat-client.onrender.com/'
+  'https://plan-eat-client.onrender.com'
 
 ];
 const corsOptions = {
